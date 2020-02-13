f8957622b4d1e0b88a3e82309970fede09dfb832
Bartosz Miąskowski <EquablePanic4@hotmail.com>
2020-02-13 15:26:20 +0100

    Początkowe zmiany

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace Codli_GCI
 {
     class Program
     {
+        private static string dataDirectory;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = $"Codli GCI [v{Assembly.GetEntryAssembly().GetName().Version}]";
+            Console.WriteLine("Welcome in Codli GCI!");
+        }
+
+        private static bool CheckPassword(string password)
+        {
+            //We need to store user's Github credintials, so it must be protected by password
+            //Password is right when first line of codli-data.shd equals "#Codli!"
+            
         }
     }
 }
