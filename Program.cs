5a8138983953a901b0e2664d2c91b4b0a3698c3e
Bartosz Miąskowski <EquablePanic4@hotmail.com>
2020-02-13 10:58:23 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Codli_GCI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
